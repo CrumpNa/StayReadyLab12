364178700b79c02e58172369055f61c5048cbf83
CrumpNa <nlcrump99@gmail.com>
2020-08-01 22:35:12 -0400

    lab 12

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,38 @@
 package io.codedifferently;
+import java.io.*;
+import java.util.*;
 
 public class OpenCloseChecker {
+
+    public Boolean isPair(String s){
+
+    Stack <Character> stack= new Stack<Character>();
+    for(int i=0;i<s.length();i++){
+        char c= s.charAt(i);
+        if(c=='('|| c=='{'|| c=='['||c=='<'||c=='\"'|| c=='\''){
+            stack.push(c); //puts this char on the stack to await its match
+        }
+        if(c==')'&& !stack.isEmpty() && stack.peek()=='('){ //if is a match
+            stack.pop(); //pops the top value in stack
+        }if(c=='}'&& !stack.isEmpty() && stack.peek()=='{') { //if is a match
+            stack.pop(); //pops the top value in stack
+        }if(c==']'&& !stack.isEmpty() && stack.peek()==']') { //if is a match
+            stack.pop(); //pops the top value in stack
+        }if(c=='>'&& !stack.isEmpty() && stack.peek()=='<') { //if is a match
+            stack.pop(); //pops the top value in stack
+        }if(c=='\"'&& !stack.isEmpty() && stack.peek()=='\"') { //if is a match
+            stack.pop(); //pops the top value in stack
+        }if(c=='\''&& !stack.isEmpty() && stack.peek()=='\'') { //if is a match
+            stack.pop(); //pops the top value in stack
+        }else{
+            //if letter, do nothing, loop continues
+        }
+
+        }
+    return stack.isEmpty();//returns bool value if stack is empty or not, if it is, output=true
+    }
+
+
+
+
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,50 @@
 package io.codedifferently;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class OpenCloseCheckerTest {
+
+    @Test
+    public void pairTestTrue(){
+        OpenCloseChecker openCloseChecker= new OpenCloseChecker();
+        String str="a(b)c(d)e(f)g";
+        Boolean expected=true;
+        Boolean actual=openCloseChecker.isPair(str);
+        //Assert.assertEquals(expected,actual);
+        Assert.assertTrue(expected);
+    }
+    @Test
+    public void pairTestTrue2(){
+        OpenCloseChecker openCloseChecker= new OpenCloseChecker();
+        String str="a{b}c<d>e\"f\"g";
+        Boolean expected=true;
+        Boolean actual=openCloseChecker.isPair(str);
+        //Assert.assertEquals(expected,actual);
+        Assert.assertTrue(expected);
+    }
+    @Test
+    public void pairTestFalse(){
+        OpenCloseChecker openCloseChecker= new OpenCloseChecker();
+        String str="a(bc(d)))";
+        Boolean expected=false;
+        Boolean actual=openCloseChecker.isPair(str);
+        //Assert.assertEquals(expected,actual);
+        Assert.assertFalse(expected);
+
+    }
+    @Test
+    public void pairTestFalse2(){
+        OpenCloseChecker openCloseChecker= new OpenCloseChecker();
+        String str="((a{b}c<d>e\"f\"g";
+        Boolean expected=false;
+        Boolean actual=openCloseChecker.isPair(str);
+        //Assert.assertEquals(expected,actual);
+        Assert.assertFalse(expected);
+    }
+
+
+
+
 
 }
